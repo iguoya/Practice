ddf1f8bb415f4cf8f9e63977b820a36e1f424541
tiger <375478250@qq.com>
2020-03-13 13:08:35 +0800

    观察者实现

---
+++ DesignPatterns/Observer/main.cpp
@@ -0,0 +1,27 @@
+#include "subject.h"
+#include "observer.h"
+#include <iostream>
+#include <fstream>
+using namespace std;
+int main(){
+    ConcreteSubject* sub = new ConcreteSubject();
+    Observer* o1 = new ConcreteObserverA(sub);
+    Observer* o2 = new ConcreteObserverB(sub);
+    sub->SetState("old");
+    sub->Notify();
+    sub->SetState("new"); //也可以由 Observer 调用
+    sub->Notify();
+
+    char buffer[256];
+    ifstream in("explanation.txt");
+    while (!in.eof() )
+    {
+        in.getline (buffer,100);
+        cout << buffer << endl;
+    }
+    in.close();
+    //    ofstream out("explanation.txt");
+    //    out<<"完全ojbk";
+    //    out.close();
+    return 0;
+}
--- DesignPatterns/Observer/subject.h
+++ DesignPatterns/Observer/subject.h
@@ -1,11 +1,34 @@
 #ifndef SUBJECT_H
 #define SUBJECT_H
 
-
-class Subject
-{
-public:
+#include <list>
+#include <string>
+using namespace std;
+typedef string State;
+class Observer;
+class Subject{
+    public:
+    virtual ~Subject();
+    virtual void Attach(Observer* obv);
+    virtual void Detach(Observer* obv);
+    virtual void Notify();
+    virtual void SetState(const State& st) = 0;
+    virtual State GetState() = 0;
+    protected:
     Subject();
+    private:
+    list<Observer* >* _obvs;
+};
+
+class ConcreteSubject:public Subject{
+     public:
+     ConcreteSubject();
+    ~ConcreteSubject();
+    State GetState();
+    void SetState(const State& st);
+    protected:
+    private:
+    State _st;
 };
 
 #endif // SUBJECT_H
