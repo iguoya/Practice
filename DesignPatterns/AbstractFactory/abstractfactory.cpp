78a7be2d0aab6a11adb25e282c7c022fa6f714b4
snow tiger <375478250@qq.com>
2020-03-18 18:52:51 +0800

    添加抽象工厂模式

--- DesignPatterns/AbstractFactory/abstractfactory.cpp
+++ DesignPatterns/AbstractFactory/abstractfactory.cpp
@@ -1,6 +1,30 @@
 #include "abstractfactory.h"
 
-AbstractFactory::AbstractFactory()
-{
-
+#include "product.h"
+#include <iostream>
+using namespace std;
+AbstractFactory::AbstractFactory(){
 }
+AbstractFactory::~AbstractFactory(){
+}
+ConcreteFactory1::ConcreteFactory1(){
+}
+ConcreteFactory1::~ConcreteFactory1(){
+}
+AbstractProductA* ConcreteFactory1::CreateProductA(){
+    return new ProductA1();
+}
+AbstractProductB* ConcreteFactory1::CreateProductB(){
+    return new ProductB1();
+}
+ConcreteFactory2::ConcreteFactory2(){
+}
+ConcreteFactory2::~ConcreteFactory2(){
+}
+AbstractProductA* ConcreteFactory2::CreateProductA(){
+    return new ProductA2();
+}
+AbstractProductB* ConcreteFactory2::CreateProductB(){
+    return new ProductB2();
+}
+
